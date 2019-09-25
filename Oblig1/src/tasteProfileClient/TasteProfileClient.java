6a13b362b0a7f48d1eb6d6ecd8bd11b47f8ad3bd
Fredrik Giertsen <A54863@hafslund.no>
2019-09-25 15:53:11 +0200

    Optimized imports

--- Oblig1/src/tasteProfileClient/TasteProfileClient.java
+++ Oblig1/src/tasteProfileClient/TasteProfileClient.java
@@ -1,12 +1,7 @@
 package tasteProfileClient;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
